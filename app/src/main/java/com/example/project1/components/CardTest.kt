bd223b71f1867943734291f8b1fee78d2fdd168a
kimiti <kimitikevin@gmail.com>
2025-12-11 22:45:44 +0300

    update testing journal screen

--- app/src/main/java/com/example/project1/components/CardTest.kt
+++ app/src/main/java/com/example/project1/components/CardTest.kt
@@ -3,12 +3,17 @@ package com.example.project1.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -21,10 +26,27 @@ import androidx.compose.ui.unit.sp
 fun CardTest() {
     Scaffold { paddingValues ->
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxWidth()
+                .padding(horizontal = 12.dp)
+                .verticalScroll(rememberScrollState())
         ) {
+            Text(
+                "All Journals",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 23.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 23.dp)
+
+            )
             CardTestItem()
             CardTestItem()
             CardTestItem()
@@ -43,10 +65,7 @@ fun CardTestItem() {
         shape = RoundedCornerShape(10.dp),
         elevation = CardDefaults.cardElevation(3.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
-        modifier = Modifier
-
-            .padding(vertical = 8.dp, horizontal = 12.dp)
-
+        modifier = Modifier.padding(vertical = 12.dp)
     ) {
         Text(
             "What are you grateful for?",
